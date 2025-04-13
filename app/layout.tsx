30c7891d9e40b305439b123c3f44e07c2733e717
SANTIAGORF24 <santiforero57@gmail.com>
2025-04-13 11:17:29 -0500

    titulo

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,9 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  title: 'Viaje de año <3',
+  description: 'Santiago Ramirez Forero',
+  generator: 'Next.js',
 }
 
 export default function RootLayout({
